0ec28bcfee909f1c9d1faf8c8ef26bb80d502a56
MarioA96 <marioaquirozs@gmail.com>
2024-09-03 15:01:39 -0600

    Cambios en la configuración de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://marioa96.github.io/',
     base: '01_FEM_recipePage'
 });
